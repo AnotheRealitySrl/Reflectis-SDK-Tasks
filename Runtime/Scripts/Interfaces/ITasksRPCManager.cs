3cc058fd016baad3740c907e6b8dd0cd0ee450a8
FrancescoWeik <fweikmann@gmail.com>
2024-03-26 18:37:14 +0100

    Update tasksRPCManager Interface

--- Runtime/Scripts/Interfaces/ITasksRPCManager.cs
+++ Runtime/Scripts/Interfaces/ITasksRPCManager.cs
@@ -1,10 +1,17 @@
 namespace Reflectis.PLG.Tasks
 {
+
+    public delegate void OnTaskComplete();
     public interface ITasksRPCManager
     {
+        public delegate void OnTaskComplete(int id);
+
+        public event OnTaskComplete OnTaskCompleted;
+
+
         public void UpdateTasksID(int id);
         public void RPC_TaskStatusChanged(int taskID);
-
         public void SendRPCTaskStatusChange(int taskID);
+        public void SetOnTaskCompleted(OnTaskComplete forceTaskComplete);
     }
 }
